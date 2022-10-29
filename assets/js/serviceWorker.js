7ed6f637d4c6993edf8f9690a053a65fa7afd770
antonin <antonin.girard.98@gmail.com>
2022-10-29 15:17:00 +0200

    :sparkles: tentative manifest & serviceWorker

---
+++ assets/js/serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevWhatWatch = "dev-what-watch-site-v1"
+const assets = [
+"/",
+"/index.html",
+"/assets/css/style.css",
+"/assets/js/main.js"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevWhatWatch).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
